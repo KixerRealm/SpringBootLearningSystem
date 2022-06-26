a05a061b099518a2787424cc27282a5aa7047ef4
Kristijan Kostovski <kixer.wade2000@outlook.com>
2022-06-26 19:05:31 +0200

    Project Created

---
+++ src/main/java/mk/ukim/finki/dnick/prototype/springbootlearningsystem/SpringBootLearningSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dnick.prototype.springbootlearningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/dnick/prototype/springbootlearningsystem/SpringBootLearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dnick.prototype.springbootlearningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
